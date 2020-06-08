b635f9c3e9e13e0f7858c489b827c0b3c2337c1f
vedantsaxena <64244974+vedantsaxena@users.noreply.github.com>
2020-06-08 11:26:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+  //create the variables for the arrays, painting, canvas and database
+  var drawing = [];
+  var currentPath = [];
+  var drawActive = false;
+  var canvas;
+  var database;
+
+
+  function setup() {
+    //create the canvas
+  canvas = createCanvas(1000,800);
+
+  //create the datbase
+  database = firebase.database();
+
+  //calling the function to draw when the mouse is pressed over the canvas
+  canvas.mousePressed(startDrawing);
+  canvas.mouseReleased(mouseStop);
+  }
+
+  function draw() {
+    background("lightgrey");
+    
+    // the coordinates of the mouse stored in a variable through the painting variable and the coords variable to the currentPath array
+    if (drawActive === true){
+      var posStorage = {
+        x:mouseX,
+        y:mouseY
+      }
+      currentPath.push(posStorage);
+    }
+    
+    //the basis for the drawing's lines
+    strokeWeight(5);
+    noFill();
+    stroke(0,150,150);
+
+  // creating the lines by using for loops , the mouse coords (through the variable in the array) and by using the vertex(),beginShape() and endShape commands
+    for (var i = 0;i<drawing.length;i++){
+      var path = drawing[i];
+      beginShape();
+    for (var j =0;j<path.length;j++){
+      vertex(path[j].x,path[j].y);
+    }
+      endShape();
+    }
+    
+  }
+
+  //the function to start the drawing
+  function startDrawing(){
+    drawActive = true;
+    currentPath = [];
+    drawing.push(currentPath);
+  }
+
+  //the function to stop drawing
+  function mouseStop(){
+    drawActive = false;
+  }
