b89e86cbc52d3ac1a403407056c4c16c83598149
vedantsaxena <tanmay.saxena@gmail.com>
2020-06-13 17:46:50 +0530

    save button and other changes made

--- sketch.js
+++ sketch.js
@@ -1,33 +1,46 @@
-  //create the variables for the arrays, painting, canvas and database
-  var drawing = [];
-  var currentPath = [];
+  //create the variables for the array, painting, canvas, database, save button and the drawing sprite and its image
+  var drawPath = [];
   var drawActive = false;
   var canvas;
   var database;
-
+  var drawMat;
+  var saveButton;
+  var drawin;
 
   function setup() {
     //create the canvas
   canvas = createCanvas(800,600);
 
-  //create the datbase
+  //create the database
   database = firebase.database();
 
-  //calling the function to draw when the mouse is pressed over the canvas
-  canvas.mousePressed(startDrawing);
-  canvas.mouseReleased(mouseStop);
+  //loading the image for the sprite through a variable
+  drawin = loadImage("images/sq.jpg");
+
+  // creating the sprite for the drawing
+  drawMat = createSprite(400,300,10,10);
+  
+//creating the save button
+  saveButton = createButton("save");
+  saveButton.position(900,560);
+
+
   }
 
   function draw() {
-    background("lightgrey");
+    background(255);
+
+   // storing the coordinates of the sprite in an array
+    var position = [drawMat.x,drawMat.y];
+
+    // storing the coordinates array in the drawPath array
+    drawPath.push(position);
     
-    // the coordinates of the mouse stored in a variable through the painting variable and the coords variable to the currentPath array
+    // the coordinates of the sprites linked with that of the mouse
     if (drawActive === true){
-      var posStorage = {
-        x:mouseX,
-        y:mouseY
-      }
-      currentPath.push(posStorage);
+      drawMat.x = mouseX;
+      drawMat.y = mouseY
+     
     }
     
     //the basis for the drawing's lines
@@ -35,26 +48,39 @@
     noFill();
     stroke(0,150,150);
 
-  // creating the lines by using for loops , the mouse coords (through the variable in the array) and by using the vertex(),beginShape() and endShape commands
-    for (var i = 0;i<drawing.length;i++){
-      var path = drawing[i];
-      beginShape();
-    for (var j =0;j<path.length;j++){
-      vertex(path[j].x,path[j].y);
-    }
-      endShape();
-    }
+  // creating the lines by using for loops and the mouse coords (through the variable in the array) 
+    for (var i = 0;i<drawPath.length;i++){
+      image(drawin,drawPath[i][0],drawPath[i][1],10,10);
+  }
+
+//the saving part when the button is pressed
+  saveButton.mousePressed(()=>{
+    database.ref("Drawer/drawingName").set({
+      personName:"Vedant",
+      drawing:"Drawing1"
+    });
+     database.ref("/").update();
+  })
+
+  drawSprites();
+}
+
+  //the function to start the drawing
+  function mouseDragged(){
+    drawActive = true;
+    database.ref("Drawer/position").set({
+      x : drawMat.x,
+      y : drawMat.y
+    })
     
   }
 
-  //the function to start the drawing
-  function startDrawing(){
-    drawActive = true;
-    currentPath = [];
-    drawing.push(currentPath);
-  }
-
   //the function to stop drawing
-  function mouseStop(){
+  function mouseReleased(){
     drawActive = false;
   }
+
+  
+   
+    
+  
